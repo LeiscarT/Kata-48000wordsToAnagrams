9fbd3a533bef94ed02c2a072e5a329152fbd4652
LeiscarT <leiscartp29@gmail.com>
2021-05-22 18:17:35 -0400

    File reader ready

--- Kata-Anagram/Kata-Anagram/FileManager.cs
+++ Kata-Anagram/Kata-Anagram/FileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Kata_Anagram
 {
@@ -10,18 +11,32 @@ namespace Kata_Anagram
     {
         private string[] privatearray = null;
 
-
-
-
-        public FileManager()
+        public FileManager() 
         {
-            
+            this.privatearray = new string[] { };
+         }
+
+
+        public FileManager(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Dirección erronea");
+            }
+
+            StreamReader sr = new StreamReader(path);
+            string line = null;
+
+            List<string> list = new List<string>();
+
+            while((line = sr.ReadLine()) != null)
+            {
+                list.Add(line);
+            }
+
+            this.privatearray = list.ToArray();
         }
 
-        public FileManager(string[] array) 
-        {
-            this.privatearray = array;
-         }
 
 
        public string [] getArray()
